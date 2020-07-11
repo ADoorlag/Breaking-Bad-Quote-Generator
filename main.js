e589211346c0c609d837aebe26e401e896556a50
ADoorlag <43080438+ADoorlag@users.noreply.github.com>
2020-07-11 17:27:55 -0400

    fixed main functionality including the startup error. quotes can now be changed at the click of a button or by refreshing the page

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-//let loading = true;
+const button = document.getElementById("generate-btn");
+const output = document.getElementById("output");
+//const phrase = document.getElementById("phrase");
+//const author = document.getElementById("author");
+let quote = "";
+
+document.addEventListener("DOMContentLoaded", getQuote);
+button.addEventListener("click", getQuote);
+
+function getQuote() {
+  fetch("https://www.breakingbadapi.com/api/quote/random")
+    .then((res) => res.json())
+    .then((data) => {
+      let results = `<h2 id="quote">"${data[0].quote}"</h2>
+                     <h3 id="author">-${data[0].author}</h3>`;
+
+      output.innerHTML = results;
+    });
+}
+
+/*let loading = true;
 const button = document.getElementById("generate-btn");
 let quotes = [];
-let quota;
 const phrase = document.getElementById("phrase");
 const author = document.getElementById("author");
+const content = document.getElementById("content");
 
 /*if (quote.innerText === "" || author.innerText === "") {
   document.getElementById("loading").className = "show";
@@ -19,23 +39,36 @@ const author = document.getElementById("author");
 } else {
   document.getElementById("loading").className = "";
   document.getElementById("content").className = "show";
-}*/
+}
 
-document.addEventListener("DOMContentLoaded", generateQuote);
-button.addEventListener("click", generateQuote);
+document.addEventListener("DOMContentLoaded", generateQuotes);
+document.addEventListener("DOMContentLoaded", insertQuote);
+button.addEventListener("click", insertQuote);
 
-function generateQuote() {
+const num = Math.floor(Math.random() * (69 - 0 + 1) + 0);
+
+function generateQuotes() {
   //quote.innerText = "";
   //author.innerText = "";
-
-  const num = Math.floor(Math.random() * (69 - 0 + 1) + 0);
 
   fetch("https://www.breakingbadapi.com/api/quotes/")
     .then((res) => res.json())
     .then((data) => (quotes = data));
   //.then((quote) => quotes[num])
   //.then(() => console.log(quote));
-  let quota = quotes[num];
-  phrase.innerText = quota.quote;
-  author.innerText = quota.author;
 }
+
+function insertQuote() {
+  //const phrase = document.createElement("h2");
+  //const author = document.createElement("h3");
+
+  //phrase.id = "phrase";
+  // author.id = "author";
+
+  let quote = quotes[num];
+  phrase.innerHTML = `${quote.quote}`;
+  author.innerHTML = `${quote.author}`;
+
+  //content.insertBefore(author, button);
+  //content.insertBefore(phrase, author);
+}*/
