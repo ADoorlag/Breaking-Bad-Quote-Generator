bdabfd98170651afcaf2c349554f1a401f145a7b
ADoorlag <43080438+ADoorlag@users.noreply.github.com>
2020-07-13 13:34:10 -0400

    main app functionality completed. Quotes are randomly generated on refresh and on button click. Loading gif also appears while quotes are being fetched

--- main.js
+++ main.js
@@ -1,22 +1,74 @@
-const button = document.getElementById("generate-btn");
-const output = document.getElementById("output");
+//const button = document.getElementById("generate-btn");
+const resultsDiv = document.getElementById("results-div");
+const content = document.getElementById("content");
+// const button = document.getElementById("generate-btn");
+//const output = document.getElementById("output");
 //const phrase = document.getElementById("phrase");
 //const author = document.getElementById("author");
-let quote = "";
 
+// document.addEventListener("fetchStart", showLoading);
+// document.addEventListener("fetchEnd", hideLoading);
 document.addEventListener("DOMContentLoaded", getQuote);
-button.addEventListener("click", getQuote);
+// button.addEventListener("click", getQuote);
 
 function getQuote() {
+  let results;
+  content.innerHTML = `<img src="images/Loading.gif" alt="Loading..." id="loader" />`;
+  // document.getElementById("loading").className = "show";
+  // document.getElementById("content").className = "";
   fetch("https://www.breakingbadapi.com/api/quote/random")
     .then((res) => res.json())
+    // .then(document.getElementById("loading").classList.remove("show"))
+    //   .then(
+    //     (document.getElementById("output").innerHTML = `<div id="output">
+    //   <div id="results-div"></div>
+    //   <button id="generate-btn">Generate another quote</button>
+    // </div>`)
+    //   )
     .then((data) => {
-      let results = `<h2 id="quote">"${data[0].quote}"</h2>
-                     <h3 id="author">-${data[0].author}</h3>`;
+      content.innerHTML = `<div id="output">
+    <div id="results-div"><h2 id="quote">"${data[0].quote}"</h2>
+                          <h3 id="author">-${data[0].author}</h3>
+                          </div>
+    <button id="generate-btn">Generate another quote</button>
+  </div>`;
 
-      output.innerHTML = results;
+      document
+        .getElementById("generate-btn")
+        .addEventListener("click", getQuote);
     });
+  // .then((data) => {
+  //   let results = `<h2 id="quote">"${data[0].quote}"</h2>
+  //                  <h3 id="author">-${data[0].author}</h3>`;
+
+  //   // resultsDiv.innerHTML = results;
+  // })
+  // .then(
+  //   document
+  //     .getElementById("generate-btn")
+  //     .addEventListener("click", getQuote)
+  // );
+  //.then((output.innerHTML = resultsDiv));
+  // .then(document.getElementById("content").classList.add("show"));
 }
+
+// function showLoading() {
+//   document.getElementById("loading").className = "show";
+//   document.getElementById("content").className = "";
+// }
+
+// function hideLoading() {
+//   document.getElementById("loading").classList.remove("show");
+//   document.getElementById("content").classList.add("show");
+// }
+
+// if (loading === true) {
+//   document.getElementById("loading").className = "show";
+//   document.getElementById("content").className = "";
+// } else {
+//   document.getElementById("loading").classList.remove("show");
+//   document.getElementById("content").classList.add("show");
+// }
 
 /*let loading = true;
 const button = document.getElementById("generate-btn");
