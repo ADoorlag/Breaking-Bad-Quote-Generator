fd26b1f77717963a20d5bd9ee9c3083bea003ba9
Alex Doorlag <Alex.Doorlag@gmail.com>
2021-05-03 13:13:00 -0400

    Finished build with a readme, licence, and comments added for clarity

--- main.js
+++ main.js
@@ -1,126 +1,56 @@
-//const button = document.getElementById("generate-btn");
+// Variable declarations
 const resultsDiv = document.getElementById("results-div");
 const content = document.getElementById("content");
-// const button = document.getElementById("generate-btn");
-//const output = document.getElementById("output");
-//const phrase = document.getElementById("phrase");
-//const author = document.getElementById("author");
-
-// document.addEventListener("fetchStart", showLoading);
-// document.addEventListener("fetchEnd", hideLoading);
+// Call getQuote function automatically when the DOM loads
 document.addEventListener("DOMContentLoaded", getQuote);
-// button.addEventListener("click", getQuote);
 
+//Function to get quotes and load them into the HTML
 function getQuote() {
-  let results;
+  // Output set to display loading gif by default while quote is fetched
   content.innerHTML = `<img src="images/Loading.gif" alt="Loading..." id="loader" />`;
-  // document.getElementById("loading").className = "show";
-  // document.getElementById("content").className = "";
+
+  //Fetch random quote from the Breaking Bad API
   fetch("https://www.breakingbadapi.com/api/quote/random")
+    //Check the status of the response and throw an error if it's not ok
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res;
+    })
+
     .then((res) => res.json())
-    // .then(document.getElementById("loading").classList.remove("show"))
-    //   .then(
-    //     (document.getElementById("output").innerHTML = `<div id="output">
-    //   <div id="results-div"></div>
-    //   <button id="generate-btn">Generate another quote</button>
-    // </div>`)
-    //   )
+
+    // Dynamically insert fetched data about the quote and the author inside of the content div
     .then((data) => {
-      content.innerHTML = `<div id="output">
-    <div id="results-div"><h2 id="quote">"${data[0].quote}"</h2>
-                          <h3 id="author">-${data[0].author}</h3>
-                          </div>
-    <button id="generate-btn">Generate another quote</button>
-  </div>`;
+      content.innerHTML = `
+      <div id="output">
+        <div id="wrap">
+          <div id="results-div">
+            <h2 id="quote">"${data[0].quote}"</h2>
+            <h3 id="author">-${data[0].author}</h3>
+          </div>
+        </div>
+        <button id="generate-btn">Generate another quote</button>
+      </div>`;
+
+      //Add event listener to the dynamically generated button that will allow another quote to be fetched
+      document
+        .getElementById("generate-btn")
+        .addEventListener("click", getQuote);
+    })
+
+    //If there is not a valid response from the API or if any error occurs, stop loading and display an error message
+    .catch((error) => {
+      content.innerHTML = `
+        <div id = "output">
+          <h1 id = "error">An error has occured. Unable to retrieve quote</h1>
+          <button id="generate-btn">Try again</button>
+        </div>
+      `;
 
       document
         .getElementById("generate-btn")
         .addEventListener("click", getQuote);
     });
-  // .then((data) => {
-  //   let results = `<h2 id="quote">"${data[0].quote}"</h2>
-  //                  <h3 id="author">-${data[0].author}</h3>`;
-
-  //   // resultsDiv.innerHTML = results;
-  // })
-  // .then(
-  //   document
-  //     .getElementById("generate-btn")
-  //     .addEventListener("click", getQuote)
-  // );
-  //.then((output.innerHTML = resultsDiv));
-  // .then(document.getElementById("content").classList.add("show"));
 }
-
-// function showLoading() {
-//   document.getElementById("loading").className = "show";
-//   document.getElementById("content").className = "";
-// }
-
-// function hideLoading() {
-//   document.getElementById("loading").classList.remove("show");
-//   document.getElementById("content").classList.add("show");
-// }
-
-// if (loading === true) {
-//   document.getElementById("loading").className = "show";
-//   document.getElementById("content").className = "";
-// } else {
-//   document.getElementById("loading").classList.remove("show");
-//   document.getElementById("content").classList.add("show");
-// }
-
-/*let loading = true;
-const button = document.getElementById("generate-btn");
-let quotes = [];
-const phrase = document.getElementById("phrase");
-const author = document.getElementById("author");
-const content = document.getElementById("content");
-
-/*if (quote.innerText === "" || author.innerText === "") {
-  document.getElementById("loading").className = "show";
-  document.getElementById("content").className = "";
-} else {
-  document.getElementById("loading").classList.remove("show");
-  document.getElementById("content").classList.add("show");
-}*/
-
-/*if (loading) {
-  document.getElementById("loading").className = "show";
-  document.getElementById("content").className = "";
-} else {
-  document.getElementById("loading").className = "";
-  document.getElementById("content").className = "show";
-}
-
-document.addEventListener("DOMContentLoaded", generateQuotes);
-document.addEventListener("DOMContentLoaded", insertQuote);
-button.addEventListener("click", insertQuote);
-
-const num = Math.floor(Math.random() * (69 - 0 + 1) + 0);
-
-function generateQuotes() {
-  //quote.innerText = "";
-  //author.innerText = "";
-
-  fetch("https://www.breakingbadapi.com/api/quotes/")
-    .then((res) => res.json())
-    .then((data) => (quotes = data));
-  //.then((quote) => quotes[num])
-  //.then(() => console.log(quote));
-}
-
-function insertQuote() {
-  //const phrase = document.createElement("h2");
-  //const author = document.createElement("h3");
-
-  //phrase.id = "phrase";
-  // author.id = "author";
-
-  let quote = quotes[num];
-  phrase.innerHTML = `${quote.quote}`;
-  author.innerHTML = `${quote.author}`;
-
-  //content.insertBefore(author, button);
-  //content.insertBefore(phrase, author);
-}*/
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Breaking Bad Quote Generator
+
+## Description
+
+This application uses JavaScript to randomly grab one of several dozen quotes from characters from the show _Breaking Bad_ and its spin-off _Better Call Saul_. Both of which are shows I and many of you probably are tremendously fond of.
+
+### How to use this application
+
+This app works automatically as soon as it's finished loading in the browser. A quote is immediately fetched from the [Breaking Bad API](https://www.breakingbadapi.com/api/). The quote and its author's name are then dynamically inserted into the HTML. Below the quote and the author is a button that when clicked will restart the whole process and fetch another quote. This will allow the user to fetch and see as many of the several dozen quotes as they desire.
+
+### Live Version
+
+The live version of this app can be found HERE
+
+### License
+
+MIT © 2021 Alex Doorlag
